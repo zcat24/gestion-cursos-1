d2344b53a9a3d1ebb54222b0f445ce2aa33574be
Juan David Mesa <juan.mesa.brito@gmail.com>
2023-04-25 16:08:12 -0500

    Initial commit

---
+++ src/main/java/com/crud/gestioncursos/GestionCursosApplication.java
@@ -0,0 +1,13 @@
+package com.crud.gestioncursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/gestioncursos/GestionCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.gestioncursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
